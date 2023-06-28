f496b957d3de330ad41a86ead752c89162243fcd
wu997198976 <997198976@qq.com>
2023-06-28 17:49:02 +0800

    “try/catch block added”

---
+++ ex2/src/dividebyzero/Main.java
@@ -0,0 +1,16 @@
+package dividebyzero;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(3/0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
